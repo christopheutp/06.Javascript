5100e93b70c6265938febf45b0e17377891c2605
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-01-11 10:23:56 +0100

    correction exercic 9

--- exercices/exercice9/main.js
+++ exercices/exercice9/main.js
@@ -1,1 +1,31 @@
-// Declarations des vaiables
+// Declarations des variables
+let indemnite  = 0, salaire =0, anciennete =0, age = 0;
+
+// Récupération et stockage des saisie utilisateur
+salaire = Number(prompt("Veuillez saisir le dernier salaire :"));
+anciennete = Number(prompt("Veuillez saisir l'ancienneté':"));
+age = Number(prompt("Veuillez saisir l'age du salarié :"));
+
+if (anciennete >= 1 && anciennete <=10 ){
+    indemnite = (salaire/2) * anciennete;
+}
+
+if(anciennete > 10) {
+    indemnite = salaire * anciennete;
+    // si on veut que les 10 premieres annés soit calculé avec un demi salaire
+    /*
+    indemnite = (salaire/2) * 10;
+    indemnite += (anciennete-10)*salaire
+    */
+}
+
+if(age >= 46 && age <=49) {
+   // indemnite = indemnite + salaire*2;
+    indemnite += salaire*2;
+}else if (age >= 50){
+    indemnite += 5*salaire;
+}
+
+
+console.log(`Votre indeminité est de ${indemnite}`)
+
