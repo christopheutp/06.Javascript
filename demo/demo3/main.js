20fd63031107ead6e4f94fa5f20fc7b4f95d34fc
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-01-12 09:52:14 +0100

    demo boucle tableaux

--- demo/demo3/main.js
+++ demo/demo3/main.js
@@ -11,9 +11,25 @@
 //     j++;
 // }
 
-let k = 11;
+// let k = 11;
 
-do {
-    console.log(`La boucle do while s'est executée ${k} fois`);
-    k++;
-}while(k <= 10)
+// do {
+//     console.log(`La boucle do while s'est executée ${k} fois`);
+//     k++;
+// }while(k <= 10)
+
+
+let jourSem = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+
+// Itération du tableau par la boucle for
+
+for(let i = 0; i< jourSem.length; i++){
+    console.log(jourSem[i]);
+  // if(jourSem[i] == "Lundi"){
+//     if(i == 0){
+//     console.log("Bon début de semaine");
+//    }
+    if(i >= 1 && i <5){
+        console.log("Bon début de semaine");
+    }
+}
