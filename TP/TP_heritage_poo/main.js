1f8ac019df211e4cc98ebd97b942057726e8acf6
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-01-30 16:54:58 +0100

    TP héritage

---
+++ TP/TP_heritage_poo/main.js
@@ -0,0 +1,5 @@
+import { Ihm } from "./classes/ihm.js";
+
+
+const ihm = new Ihm(document.querySelector('#formArticle'),document.querySelector('#resultProduit'),document.querySelector('#resultService'))
+ihm.demarrer();
